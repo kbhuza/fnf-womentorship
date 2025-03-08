07d82d2dec5ad5f3552d045a01050ff9044dbf86
bongani dube <bryandube836@gmail.com>
2025-03-08 14:04:30 +0200

    language

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,12 +1,12 @@
 import 'package:fnf_womentorship/src/pages/authentication/sign_in.dart';
+import 'package:fnf_womentorship/src/pages/language/select_language_page.dart';
 import 'package:fnf_womentorship/src/pages/splashscreen/splash_screen.dart';
 import 'package:get/get.dart';
 
-
-class AppRoutes{
-  static List<GetPage>routes()=>[
-    
-        GetPage(name: "/", page: ()=> const SplashScreen()),
-        GetPage(name: SignIn.routeName, page: ()=> SignIn()),
-  ];
+class AppRoutes {
+  static List<GetPage> routes() => [
+        GetPage(name: "/", page: () => const SplashScreen()),
+        GetPage(name: SignIn.routeName, page: () => SignIn()),
+        GetPage(name: SelectLanguagePage.routeName, page: () => SelectLanguagePage()),
+      ];
 }
